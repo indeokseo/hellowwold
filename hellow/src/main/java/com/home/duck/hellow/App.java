72eea115ced3b733b1968cbe10df2fc9bae5f091
sid0206 <sid0206@igloosec.com>
2018-11-21 16:22:30 +0900

    hellow

---
+++ hellow/src/main/java/com/home/duck/hellow/App.java
@@ -0,0 +1,10 @@
+package com.home.duck.hellow;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ hellow/src/main/java/com/home/duck/hellow/hellowduck.java
@@ -0,0 +1,8 @@
+package com.home.duck.hellow;
+
+public class hellowduck {
+
+public static void main (String [] args) {
+	System.out.println("hi");
+}
+}
